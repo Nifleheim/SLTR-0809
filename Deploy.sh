c9007e9362a7eabc372de00d0b02f190c10efa6c
Reza Fabryan <123403871+Nifleheim@users.noreply.github.com>
2023-09-09 18:46:54 +0700

    Create Deploy.sh

---
+++ Deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo pip install flask -y
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh --dry-run
+sudo apt-get update -y
+sudo apt install docker.io -y
+sudo pip install fastapi
+sudo pip install uvicorn
+docker pull ikanpaus/simplepapptest:1.0.0
+docker run -p 8000:5000 ikanpaus/simplepapptest:1.0.0
