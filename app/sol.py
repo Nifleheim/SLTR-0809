0c3779b529038da0b7059c03ce1ed63827b44636
Reza Fabryan <123403871+Nifleheim@users.noreply.github.com>
2023-09-10 10:18:12 +0700

    Update test.py

--- app/sol.py
+++ app/sol.py
@@ -1,1 +1,22 @@
+from typing import Optional
 
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Selamat datang ke halaman utama Solitaire Take Home Test!"
+
+@app.get("/welcome/")
+def anonymous():
+    return "Are you Anonymous???"
+
+@app.get("/welcome/{name}")
+def welcome(name):
+    return f"Selamat datang {name}!"
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=5000, host="0.0.0.0")
